1ea6027737135d2688276b881dc7bee805f44484
Diegol2023 <logicadigital@hotmail.com>
2025-05-09 12:56:40 -0300

    first commit

---
+++ agenda.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>AGENDA - iNTERNOS</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="header">
+                <h1>AGENDA</h1>
+                <h2>INTERNOS</h2>
+            </div>
+            <div class="content">
+                <div class="table">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Apellido</th>
+                                <th>Sector</th>
+                                <th>Telefono Interno</th>
+                                <th>Email</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>    </td>
+                                <td>    </td>
+                                <td>    </td>
+                                <td>    </td>
+                                <td>    </td>
+                                <td>    </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </body>
+</html> 
